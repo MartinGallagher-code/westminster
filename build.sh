c1f8b685b959140d3dc51f0803649fb46e13a808
MartinGallagher-code <martinjgallagher@icloud.com>
2026-02-20 15:30:06 -0500

    watson

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ python manage.py migrate
 # Load catechism data (idempotent - safe to run on every deploy)
 python manage.py load_catechism
 python manage.py load_fisher
+python manage.py load_watson
 python manage.py load_vincent
 python manage.py load_prooftexts
 python manage.py fetch_scripture --delay=0.3
